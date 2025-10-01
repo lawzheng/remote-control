d6b0f8579987561f4d23307fa179f2784b4bf02e
lawzheng <602285510@qq.com>
2025-10-01 21:26:07 +0800

    添加基础通信

--- app/main/index.js
+++ app/main/index.js
@@ -1,20 +1,8 @@
-const { app, BrowserWindow } = require('electron')
-const isDev = require('electron-is-dev')
-const path = require('path')
+const { app } = require('electron')
+const handleIpc = require('./ipc')
+const { create: createMainWindow } = require('./windows/main')
 
-let win
 app.on('ready', () => {
-  win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-    },
-  })
-  if (isDev) {
-    win.loadURL('http://localhost:5173/')
-  } else {
-    win.loadFile(path.resolve(__dirname, '../renderer/pages/main/index.html'))
-  }
+  createMainWindow();
+  handleIpc();
 })
--- app/renderer/src/main/src/App.jsx
+++ app/renderer/src/main/src/App.jsx
@@ -1,36 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-// import { ipcRenderer } from 'electron'
+import { useState, useEffect } from "react";
+import "./App.css";
+const { ipcRenderer } = window.require("electron");
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [remoteCode, setRemoteCode] = useState("");
+  const [localCode, setLocalCode] = useState("");
+  const [controlText, setControlText] = useState("");
+  const login = async () => {
+    const code = await ipcRenderer.invoke("login");
+    setLocalCode(code);
+  };
+  const handleControlState = (e, name, type) => {
+    let text = "";
+    if (type === 1) {
+      text = `正在远程控制${name}`;
+    } else if (type === 2) {
+      text = `被${name}控制中`;
+    } else {
+      text = "";
+    }
+    setControlText(text);
+  };
+  const startControl = (remoteCode) => {
+    ipcRenderer.send("control", remoteCode);
+  };
+  
+  useEffect(() => {
+    login();
+    ipcRenderer.on("control-state-change", handleControlState);
+    return () => {
+      ipcRenderer.removeListener("control-state-change", handleControlState);
+    };
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      {controlText === "" ? (
+        <>
+          <div>你的控制码{localCode}</div>
+          <input
+            type="text"
+            value={remoteCode}
+            onChange={(e) => setRemoteCode(e.target.value)}
+          />
+          <button onClick={() => startControl(remoteCode)}>确认</button>
+        </>
+      ) : (
+        <div>{controlText}</div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
